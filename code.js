24eb740b7386fde04ca0c49f2372f8f289ff6602
Aadika201 <74857921+Aadika201@users.noreply.github.com>
2022-06-24 14:16:16 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+var firstname="Aadika";
+ console.log(firstname);
+var lastname="Thiruvaipati";
+ console.log(lastname);
+var fullname=firstname.concat(lastname);
+ console.log(fullname);
+//Math
+ var square_root=Math.sqrt(25);
+ console.log(square_root);
+ var maximum_number= Math.max(15, 10, 25);
+ console.log(maximum_number);
+ var minimum_number=Math.min(15, 10, 25);
+ console.log(minimum_number);
+ var power= Math.pow(2,3);
+ console.log(power);
